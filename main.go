e0aee048091c2ba5aa8e5e8d30bc488d88a2fd2f
Nurio Fernandez <xxnurioxx@gmail.com>
2026-05-16 12:06:09 +0200

    Implemented scheduler

--- main.go
+++ main.go
@@ -8,21 +8,24 @@ import (
 
 	"ddns/cloudflare"
 	"ddns/ipinfo"
+	"ddns/scheduler"
 )
 
 func main() {
-	fmt.Println("[DDNS] Attempting to update DDNS...")
-	ip, err := ipinfo.FetchIp()
-	if err != nil {
-		fmt.Println("[DDNS] Unable to fetch current IP address.")
-		log.Fatal(err)
-	}
+	scheduler.Schedule(func() {
+		fmt.Println("[DDNS] Attempting to update DDNS...")
+		ip, err := ipinfo.FetchIp()
+		if err != nil {
+			fmt.Println("[DDNS] Unable to fetch current IP address.")
+			log.Fatal(err)
+		}
 
-	err = cloudflare.Update(ip)
-	if err != nil {
-		fmt.Println("[DDNS] Unable to update Cloudflare DNS record.")
-		log.Fatal(err)
-	}
+		err = cloudflare.Update(ip)
+		if err != nil {
+			fmt.Println("[DDNS] Unable to update Cloudflare DNS record.")
+			log.Fatal(err)
+		}
 
-	fmt.Println("[DDNS] Successfully updated DDNS record!")
+		fmt.Println("[DDNS] Successfully updated DDNS record!")
+	})
 }
---
+++ scheduler/handler.go
@@ -0,0 +1,45 @@
+package scheduler
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+func Schedule(executableTask func()) {
+	repeatInterval := parseMinutes()
+	if repeatInterval != 0 {
+		fmt.Printf("[DDNS] [Scheduler] Running every %d minutes...\n", repeatInterval)
+	}
+
+	// Execute task (both once & scheduled)
+	executableTask()
+
+	// Setup scheduler
+	if repeatInterval != 0 {
+		ticker := time.NewTicker(time.Duration(repeatInterval) * time.Minute)
+		defer ticker.Stop()
+
+		for range ticker.C {
+			executableTask()
+			fmt.Printf("[DDNS] [Scheduler] Next update will be in %d minutes...\n", repeatInterval)
+		}
+	}
+}
+
+func parseMinutes() int {
+	var intervalMinutes = os.Getenv("INTERVAL_MINUTES")
+	if intervalMinutes == "" {
+		return 0
+	}
+
+	// Parse string to minutes
+	minutes, err := strconv.Atoi(intervalMinutes)
+	if err != nil || minutes <= 0 {
+		fmt.Printf("[DDNS] [Scheduler] Invalid interval '%s'.\n", intervalMinutes)
+		return 0
+	}
+
+	return minutes
+}
